8d37685a7dfd7fa627330427f06e80d5e47f8149
Nawal Ali <mnawalali4@gmail.com>
2021-11-11 23:37:02 +0500

    Create GRID.java

---
+++ BIN/Grid/GRID.java
@@ -0,0 +1,33 @@
+package Grid;
+public class GRID {
+    Cell Cells[][];
+    int Rows;
+    int Coloumns;
+    GridHandler GridHandler;
+
+    public GRID(int Rows, int Coloumns)
+    {
+        this.Rows=Rows;
+        this.Coloumns=Coloumns;
+    }
+    
+    public Cell getGrid()
+    {
+        //
+    }
+
+    public int getRows()
+    {
+        return Rows;
+    }
+
+    public int getColoumns()
+    {
+        return Coloumns;
+    }
+
+    public GridHandler GridHandler()
+    {
+        //code here
+    }
+}

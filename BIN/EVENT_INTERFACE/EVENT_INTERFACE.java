a72232bb6225f04998c63889084cfdbea76cf976
Nawal Ali <mnawalali4@gmail.com>
2021-11-12 00:16:39 +0500

    new event and storage interfaces added

---
+++ BIN/EVENT_INTERFACE/EVENT_INTERFACE.java
@@ -0,0 +1,12 @@
+package EVENT_INTERFACE;
+
+public interface EVENT_INTERFACE 
+{
+public void onClickEvents();
+
+public void onCursorChangeEvents();
+
+public void onKeyPressedEvents();
+
+public void onWindowCloseEvents();
+}
---
+++ BIN/STORAGE_INTERFACE/STORAGE_INTERFACE.java
@@ -0,0 +1,11 @@
+package STORAGE_INTERFACE;
+
+public interface STORAGE_INTERFACE {
+    public void viewStates();
+
+    public void loadState();
+
+    public void saveState();
+
+    public void deleteState();
+}

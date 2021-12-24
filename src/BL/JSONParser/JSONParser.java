900e84d74bd1d2d738d718e66eddadc32a9461c0
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2021-12-24 22:40:23 +0500

    Started Phase 4

--- src/BL/JSONParser/JSONParser.java
+++ src/BL/JSONParser/JSONParser.java
@@ -1,5 +1,19 @@
 package src.BL.JSONParser;
 
+import org.json.simple.JSONObject;
+
 public class JSONParser {
-    
+
+    JSONObject Object;
+
+    JSONParser()
+    {
+        Object = new JSONObject();
+    }
+
+    JSONObject StringTOJSON(String JSON)
+    {
+        /// Add code here.... e.g. put/add
+        return Object;
+    }
 }

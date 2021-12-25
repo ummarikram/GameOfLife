66b263139d68912a04b9a41026aba6944e2269b8
Nawal Ali <mnawalali4@gmail.com>
2021-12-25 20:57:16 +0500

    json work

--- src/BL/JSONParser/JSONParser.java
+++ src/BL/JSONParser/JSONParser.java
@@ -1,10 +1,12 @@
-package src.BL.JsonParser;
+package src.BL.JSONParser;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import src.BL.Grid.Grid;
 import src.BL.Grid.Grid.Cell;
+
+import java.util.ArrayList;
 
 public class JsonParser {
 
@@ -57,5 +59,93 @@ public class JsonParser {
 
         return grid;
     }
+
+    public JSONObject STRINGTOJSON (String string, String key )
+    {
+        JSONObject object = new JSONObject();
+      
+        
+            object.put(key,string);
+        
+
+        return object;
+    }
+
+    public String JSONTOSTRING (JSONObject object, String key)
+    {
+     
+        String string=(String) object.get(key);
+        
+        return string;
+    }
+
+    public JSONObject INTTOJSON (int val, String key)
+    {
+        JSONObject obj = new JSONObject();
+        obj.put(key, val);
+        return obj;
+
+    }
+
+    public int JSONTOINT (JSONObject object,String key)
+    {
+        int temp =(int) object.get("int");
+        return temp;
+    }
+
+    public JSONObject BOOLTOJSON (boolean val, String key)
+    {
+        JSONObject obj = new JSONObject();
+        obj.put(key, val);
+        return obj;
+    }
+
+    public boolean JSONTOBOOLEAN (JSONObject object, String key)
+    {
+        boolean bool=(boolean) object.get(key);
+        return bool;
+    }
+
+
+    public JSONObject ArraylistTOjson(ArrayList<String> arr)
+    {
+        JSONObject object = new JSONObject();
+        
+        for (int i = 0; i < arr.size(); i++) 
+        {
+            object.put(String.valueOf(i), arr.get(i));
+              
+        }
+ 
+        return object;
+    }
+    public ArrayList<String> JsonTOarrlist(JSONObject object)
+    {
+
+        ArrayList<String> arr = new  ArrayList<String>();
+    
+        for (int i = 0; i < object.size(); i++) 
+        {
+           arr.add((String) object.get(String.valueOf(i)));
+        }        
+
+        return arr;
+
+    }
+
+    public  JSONObject concatjson (JSONObject obj1, String obj1key, JSONObject obj2, String obj2key)
+    {
+        
+        JSONObject combined = new JSONObject();
+        combined.put(obj1key, obj1);
+        combined.put(obj2key, obj2);
+
+       // JSONObject newobj= new JSONObject();
+        //newobj.put(newkey, combined);
+
+        return combined;
+    } 
+
+
 
 }

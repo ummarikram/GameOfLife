1b5bdfb39ad8bd3de4e794088763ef9720e63bfe
Ummar Ikram <59392080+ummarikram@users.noreply.github.com>
2021-12-26 22:50:31 +0500

    Phase 3 Completed

--- src/Entry.java
+++ src/Entry.java
@@ -1,9 +1,9 @@
 package src;
 
 import src.BL.LogicLayer.*;
-import src.Storage.DatabaseHandler.*;
+//import src.Storage.DatabaseHandler.*;
 import src.Storage.FileHandler.*;
-import src.UI.Console.*;
+// import src.UI.Console.*;
 import src.UI.Graphical.*;
 
 
@@ -12,18 +12,18 @@ public class Entry
     public static void main(String[] args)
     {   
 
-        LogicLayer Logic = new LogicLayer(0,0);
+        LogicLayer Logic = new LogicLayer();
         
 
         FileHandler fileHandler = new FileHandler();
-        DatabaseHandler databaseHandler = new DatabaseHandler();
+        //DatabaseHandler databaseHandler = new DatabaseHandler();
 
 
         Graphical GUI = new Graphical(Logic, Logic, fileHandler);
         GUI.Display();
 
-       // Console cui = new Console(Logic, Logic, fileHandler);
-      //  cui.Display();
+         //Console cui = new Console(Logic, Logic, databaseHandler);
+         //cui.Display();
     
     }
 }

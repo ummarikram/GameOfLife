8dd1a232be51cb3d97dd7cfbeb5cf34e13040f98
Nawal Ali <mnawalali4@gmail.com>
2021-12-26 23:55:17 +0500

    completed phase 3

--- src/Entry.java
+++ src/Entry.java
@@ -2,33 +2,24 @@ package src;
 
 import src.BL.LogicLayer.*;
 import src.Storage.DatabaseHandler.*;
-//import src.Storage.FileHandler.*;
+import src.Storage.FileHandler.*;
 import src.UI.Console.*;
-//import src.UI.Graphical.*;
+import src.UI.Graphical.*;
 
-
-public class Entry 
-{	
-    public static void main(String[] args)
-    {   
+public class Entry {
+    public static void main(String[] args) {
 
         LogicLayer Logic = new LogicLayer();
+
         
+       // FileHandler fileHandler = new FileHandler(Logic);
+         DatabaseHandler databaseHandler = new DatabaseHandler(Logic);
 
-        //FileHandler fileHandler = new FileHandler();
-        DatabaseHandler databaseHandler = new DatabaseHandler();
+        Graphical GUI = new Graphical(Logic, Logic, databaseHandler, Logic);
+        GUI.Display();
 
+        // Console cui = new Console(Logic, Logic, fileHandler);
+        // cui.Display();
 
-        //Graphical GUI = new Graphical(Logic, Logic, databaseHandler);
-        //GUI.Display();
-
-        Console cui = new Console(Logic, Logic, databaseHandler);
-        cui.Display();
-    
     }
 }
-
-
-
-
-
